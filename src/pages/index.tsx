ad5172b458fd0d097dfaf96dde1c37e6472053a0
Bordignon Schuvantek Nunes <robu@gft.com>
2022-02-07 23:56:05 -0300

    class 03 - first

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,45 @@
-export default function Home() {
+import { format, parseISO } from "date-fns";
+import { ptBR } from "date-fns/locale";
+import { GetStaticProps } from "next";
+import { api } from "../services/api";
+import { Episodes } from "../types/types";
+import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
+interface HomeProps {
+  episodes: Episodes[];
+}
+
+export default function Home(props: HomeProps) {
   return <h1>Hello</h1>;
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get("episodes", {
+    params: {
+      _limit: 12,
+      _sort: "published_at",
+      _order: "desc",
+    },
+  });
+
+  const episodes = data.map((episode) => ({
+    id: episode.id,
+    title: episode.title,
+    thumbnail: episode.thumbnail,
+    members: episode.members,
+    publishedAt: format(parseISO(episode.published_at), "d MMM yy", {
+      locale: ptBR,
+    }),
+    duration: Number(episode.file.duration),
+    durationAsString: convertDurationToTimeString(
+      Number(episode.file.duration)
+    ),
+    url: episode.file.url,
+  }));
+
+  return {
+    props: {
+      episodes: episodes,
+    },
+    revalidate: 60 * 60 * 8,
+  };
+};
---
+++ src/types/types.ts
@@ -0,0 +1,10 @@
+export interface Episodes {
+  id: string;
+  title: string;
+  thumbnail: string;
+  members: string;
+  duration: number;
+  durationAsString: string;
+  url: string;
+  publishedAt: string;
+}
